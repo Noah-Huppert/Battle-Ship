c13dc2542d6329d0f57bb32a4740082fc75e646a
block7 <block7@B339-Computer-Lab-44.local>
2014-12-04 14:22:57 -0500

    Change BattleShip to public static main

--- src/com/noahhuppert/battleship/BattleShipMain.java
+++ src/com/noahhuppert/battleship/BattleShipMain.java
@@ -4,23 +4,17 @@ import com.noahhuppert.battleship.helpers.Log;
 import com.noahhuppert.battleship.models.players.Player;
 import com.noahhuppert.battleship.models.players.UserInputPlayer;
 import com.noahhuppert.battleship.views.MainView;
-import javafx.application.Application;
-import javafx.stage.Stage;
-
-import javax.swing.*;
-import java.awt.*;
 
 /**
  * Created by block7 on 12/2/14.
  */
-public class BattleShipMain extends Application{
+public class BattleShipMain{
     private static Player humanPlayer = new Player("Noah");
     private static Player computerPlayer = new Player("CPU");
     private static MainView mainView;
     private int turnNumber = 1;
 
-    @Override
-    public void start(Stage stage){
+    public static void main(String args[]){
         MainView mainView = new MainView();
 
         //mainView.getGuessTextPane().setText(humanPlayer.getGuessGrid().toString());
@@ -39,12 +33,5 @@ public class BattleShipMain extends Application{
         out += "Entering nothing will allow you to act for your turn";
 
         Log.noTag(out);
-    }
-
-    public static void act(String choice){
-        humanPlayer.act(computerPlayer, choice);
-
-        mainView.getGuessTextPane().setText(humanPlayer.getGuessGrid().toString());
-        mainView.getSelfTextPane().setText(humanPlayer.getSelfGrid().toString());
     }
 }
