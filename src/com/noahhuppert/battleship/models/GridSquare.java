857972c7c6644254b0070a1ed055954ec4f6b19b
block7 <block7@B339-Computer-Lab-44.local>
2014-12-08 13:37:08 -0500

    Made it tell you when you sink a ship

--- src/com/noahhuppert/battleship/models/GridSquare.java
+++ src/com/noahhuppert/battleship/models/GridSquare.java
@@ -24,6 +24,7 @@ public class GridSquare {
 
             if(shipDestroyed) {
                 setShip(null);
+                Log.i("Ship sunk!");
             }
 
             setHitStatus(HitStatus.HIT);
